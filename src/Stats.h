c686becd261a41abb0c050eabf75b2826e4b711e
Aksel <aksel@localhost>
2026-03-16 18:38:32 +0000

    Add GameStats structure for tracking statistics
    Tracks:
    - Games played
    - Total score across all games
    - High score
    - Total food eaten
    - Longest snake achieved
    
    Will be used for persistent statistics tracking.

---
+++ src/Stats.h
@@ -0,0 +1,21 @@
+#ifndef STATS_H
+#define STATS_H
+
+#include <string>
+
+struct GameStats {
+    int gamesPlayed = 0;
+    int totalScore = 0;
+    int highScore = 0;
+    int totalFoodEaten = 0;
+    int longestSnake = 0;
+    
+    void reset() {
+        gamesPlayed = 0;
+        totalScore = 0;
+        totalFoodEaten = 0;
+        longestSnake = 0;
+    }
+};
+
+#endif
